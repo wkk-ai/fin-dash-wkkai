619319bb68e93b8a039cf55d005dfaa8df9c568a
William kazuo <willkulminare@gmail.com>
2026-03-01 15:48:34 -0300

    Added fead-in animation when switching tabs

--- app/template.tsx
+++ app/template.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Template({ children }: { children: ReactNode }) {
+    const pathname = usePathname();
+
     return (
-        <div className="page-transition min-h-full flex flex-col">
+        <div key={pathname} className="page-transition min-h-full flex flex-col">
             {children}
         </div>
     );
